81f412ce27763f7444c7955d402ab796583df972
sgaribottounsam <sgaribotto@unsam.edu.ar>
2025-10-27 13:44:41 +0000

    Ahora sí los arreglé

--- Academica DB.session.sql
+++ Academica DB.session.sql
@@ -1,15 +1,1 @@
-SELECT
-            ic.anio, -- Usar el año académico correcto
-            strftime('%m-%d', ic.fecha_insc) AS dia_mes,
-            COUNT(ic.n_documento) AS cantidad
-        FROM inscripciones_carreras AS ic
-        JOIN propuestas AS p ON ic.carrera = p.codigo
-        WHERE ic.anio = 2025 -- Filtrar por año académico
-          AND (
-            (strftime('%m', ic.fecha_insc) = '10') OR
-            (strftime('%m', ic.fecha_insc) = '11') OR
-            (strftime('%m', ic.fecha_insc) = '12' AND strftime('%d', ic.fecha_insc) <= '30')
-          )
-          AND p.tipo = 'Grado'
-        GROUP BY ic.anio, dia_mes
-        ORDER BY ic.anio, dia_mes
+DELETE FROM inscripciones_carreras WHERE anio = 2025;

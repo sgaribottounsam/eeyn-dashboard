9b883e4bdf656691bec75c0027641a9bffae7689
sgaribottounsam <sgaribotto@unsam.edu.ar>
2025-11-06 20:11:55 +0000

    Inclusión de posgrado en la base de datos académica

--- data/base_de_datos/consultas/estudiantes_activos.sql
+++ data/base_de_datos/consultas/estudiantes_activos.sql
@@ -1,6 +1,8 @@
-SELECT substr(periodo, 1, 4) AS anio, carrera,
+SELECT substr(periodo, 1, 4) AS anio, c.tipo,
          COUNT(DISTINCT identificacion) AS total_estudiantes
 FROM inscripciones_cursadas
+LEFT JOIN propuestas as c
+    ON inscripciones_cursadas.carrera = c.codigo
 WHERE estado_insc = 'Aceptada'
-    AND carrera NOT IN ('MA-MEDT-P', 'PR-MPCC-P', 'TE-GUIA-P', 'TG-EEYN-P', 'ES-EGTI-P')
-GROUP BY anio
+    AND c.tipo NOT IN ('Cursos', 'Curso de Ingreso', 'Vocacional')
+GROUP BY anio, c.tipo
